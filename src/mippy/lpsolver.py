18ad665103111e88a2ae17dd304717f05aca0ecb
joshuaalbert <albert@strw.leidenuniv.nl>
2017-07-17 14:15:33 +0200

    typo

--- src/mippy/lpsolver.py
+++ src/mippy/lpsolver.py
@@ -206,22 +206,4 @@ class LPSolver(object):
         data = np.loadtxt(result_file)[:,1]
         return data
 
-        
-
-if __name__ == '__main__':
-    import numpy as np
-    rows = 500
-    cols = 50
-    A_eq = np.random.normal(size=[rows,cols])
-    b_eq = np.random.normal(size=rows)
-    #print(np.linalg.inv(A_eq).dot(b_eq))
-    A_lt = None
-    b_lt = None
-    A_gt = None
-    b_gt = None
-    maximize = True
-    problem_name = "TEST"
-    c_obj = np.zeros(cols)
-    l = LPSolver(A_eq, b_eq, A_lt, b_lt, A_gt, b_gt, c_obj, maximize, problem_name, 'SIMP')
-    prob = l.compile()
-    l.submit_problem(prob)
+ 
